b99f94c0b8f82675920e9aed07aee9682a42d073
Ankit Nigam <ankit.nigam@tothenew.com>
2024-01-10 11:49:03 +0530

    Created Initial Project

---
+++ LearningWidgetKit/LearningWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningWidgetKitApp.swift
+//  LearningWidgetKit
+//
+//  Created by Ankit Nigam on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
